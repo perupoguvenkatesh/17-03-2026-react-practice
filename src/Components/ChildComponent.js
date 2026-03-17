681acd8e21318c897c3d09199b268d32d7979054
Ajaygoskula <ajaygoskula799@gmail.com>
2026-03-17 12:04:51 +0530

    Added extra lines by Ajay

--- src/Components/ChildComponent.js
+++ src/Components/ChildComponent.js
@@ -3,7 +3,10 @@ import React from 'react'
 function ChildComponent() {
   return (
     <div>
-        <h1>Ajay</h1>
+        <h1>Ajay Goskula</h1>
+        <h1>Ajay is bad boy</h1>
+        <h1>He Is very naughty</h1>
+
     </div>
   )
 }
